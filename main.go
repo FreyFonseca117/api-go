1380ae68e5ce6f278c20ecfa31e2936f7ab1835c
Frey <freyfonseca@hotmail.com>
2025-06-10 22:44:02 -0400

    api rest completa, falta put delete

--- main.go
+++ main.go
@@ -1,20 +1,64 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
 
-type Album struct {
-	ID     int    `json:"id"`
+	"github.com/gin-gonic/gin"
+)
+
+// albun Estructura de albun
+type album struct {
+	ID     string `json:"id"`
 	Title  string `json:"title"`
 	Artist string `json:"artist"`
 	Year   int    `json:"year"`
 }
 
-// Variable global (declaración válida fuera de funciones)
-var albums = []Album{
-	{ID: 1, Title: "El Mal Querer", Artist: "Rosalía", Year: 2018},
-	{ID: 2, Title: "Colores", Artist: "J Balvin", Year: 2020},
+// Lista de albuns
+var albums = []album{
+	{ID: "1", Title: "Familia", Artist: "Camila Cabello", Year: 2022},
+	{ID: "2", Title: "21", Artist: "Adele", Year: 2011},
+	{ID: "3", Title: "The Eminem Show", Artist: "Eminem", Year: 2002},
+	{ID: "4", Title: "Meteora", Artist: "Linkin Park", Year: 2003},
+	{ID: "5", Title: "25", Artist: "Adele", Year: 2015},
+}
+
+// Obtener lista de albums
+func getAlbums(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, albums)
+}
+
+// Agregar un albums
+func postAlbums(c *gin.Context) {
+	var newAlbum album
+
+	if err := c.BindJSON(&newAlbum); err != nil {
+		return
+	}
+
+	albums = append(albums, newAlbum)
+	c.IndentedJSON(http.StatusCreated, newAlbum)
+}
+
+// Obtener po id un albuns
+func getAlbumByID(c *gin.Context) {
+	id := c.Param("id")
+
+	for _, a := range albums {
+		if a.ID == id {
+			c.IndentedJSON(http.StatusOK, a)
+			return
+		}
+	}
+
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Albun no encontrado"})
 }
 
 func main() {
-	fmt.Println(albums)
+	router := gin.Default()
+	router.GET("/albums", getAlbums)
+	router.POST("/albums", postAlbums)
+	router.GET("/albums/:id", getAlbumByID)
+
+	router.Run("localhost:8080")
 }
